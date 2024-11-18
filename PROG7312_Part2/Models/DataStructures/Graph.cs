7ccca4eea5cdeffea7a7ddf243ede189394f217b
Tristan Stavast <104350793+TristanStavast@users.noreply.github.com>
2024-11-18 13:25:30 +0200

    Completed basic DataStructures folder for this part

---
+++ PROG7312_Part2/Models/DataStructures/Graph.cs
@@ -0,0 +1,30 @@
+﻿using PROG7312_Part2.Models.DataStructures;
+using System.Collections.Generic;
+
+
+namespace PROG7312_Part2.Models.DataStructures
+{
+    public class Graph
+    {
+        private readonly Dictionary<int, List<int>> _adjacenctList;
+
+        public Graph()
+        {
+            _adjacenctList = new Dictionary<int, List<int>>();
+        }
+
+        public void AddEdge(int from, int to)
+        {
+            if(!_adjacenctList.ContainsKey(from))
+            {
+                _adjacenctList[from] = new List<int>();                
+            }
+            _adjacenctList[from].Add(to);
+        }
+
+        public List<int> GetDependencies(int id)
+        {
+            return _adjacenctList.ContainsKey(id) ? _adjacenctList[id] : new List<int>();
+        }
+    }
+}
